d75de07bdec30c2abe65a14d3a095b031548728c
byung82 <byung82@gmail.com>
2015-01-11 16:31:51 +0900

    image processor 추가

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -38,7 +38,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     #process :scale => [50, 50]
-    process :resize_to_fit => [678]
+    process :resize_to_fit => [678, -1]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
